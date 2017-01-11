077eebb426dffb086ad6ed61ef954aa2550a7445
Gilbert Akpan <gilbertakpan@silver-surfer.apollosrc.root>
2017-01-11 10:18:03 -0600

    purchases-clojure

---
+++ src/purchases_clojure/core.clj
@@ -0,0 +1,24 @@
+(ns purchases-clojure.core
+  (:require [clojure.string :as str])
+  (:gen-class))
+
+(defn -main []
+  (println "Type category name:")
+  (let [purchases (slurp "purchases.csv")
+        purchases (str/split-lines purchases)
+        purchases (map (fn [line]
+                         (str/split line #","))
+                    purchases)
+        header (first purchases)
+        purchases (rest purchases)
+        purchases (map (fn [line]
+                         (zipmap header line))
+                    purchases)
+        category-type (read-line)
+        purchases (filter (fn [line]
+                            ( = (get line "category") category-type))
+                    purchases)
+        file-text (pr-str purchases)]
+    (spit "filtered_purchases.edn" file-text)))
+
+
